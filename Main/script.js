689b9c92e02a0e3dc18421f9f418f90a61a93eb1
bradleytevans <bradleyt.evans@gmail.com>
2021-12-30 21:21:28 -0500

    finished adding main code to sript branch

--- Main/script.js
+++ Main/script.js
@@ -4,11 +4,41 @@ var weatherAPIKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
 var todayCon = document.querySelector('#today');
-var forcastCon = document.querySelector('#forecast');
+var forecastCon = document.querySelector('#forecast');
 var searchHistoryContainer = document.querySelector('#history');
 
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
+
+function createSearchHistory() {
+	searchHistoryContainer.innerHTML = '';
+	for (var i = searchHistory.length - 1; i >= 0; i--) {
+		var btn = document.createElement('button');
+		btn.setAttribute('type', 'button');
+		btn.setAttribute('aria-controls', 'today forecast');
+		btn.classList.add('history-btn', 'btn-history');
+		btn.setAttribute('data-search', searchHistory[i]);
+		btn.textContent = searchHistory[i];
+		searchHistoryContainer.append(btn);
+	}
+}
+
+function updateHistory(search) {
+	if (searchHistory.indexOf(search) !== -1) {
+		return;
+	}
+	searchHistory.push(search);
+	localStorage.setItem('search-history', JSON.stringify(searchHistory));
+	createSearchHistory();
+}
+
+function getSearchHistory() {
+	var storedHistory = localStorage.getItem('search-history');
+	if (storedHistory) {
+		searchHistory = JSON.parse(storedHistory);
+	}
+	createSearchHistory();
+}
 
 function currentWeather(city, weather, timezone) {
     var date = dayjs().tz(timezone).format('M/D/YYYY');
@@ -42,7 +72,7 @@ function currentWeather(city, weather, timezone) {
     tempEl.textContent = `Temp: ${temp}°F`;
     windEl.textContent = `Wind: ${windSpeed}mph`;
     humidEl.textContent = `Humidity: ${humidity} %`;
-    cardBody.append(heading, temp, windEl, humidEl);
+    cardBody.append(heading, tempEl, windEl, humidEl);
     uviEL.textContent = 'UV Index: ';
     uviBadge.classList.add('btn', 'btn-sm');
     if (uvi < 3) {
@@ -57,36 +87,6 @@ function currentWeather(city, weather, timezone) {
     cardBody.append(uviEL);
     todayCon.innerHTML = '';
     todayCon.append(card);
-}
-
-function createSearchHistory() {
-	searchHistoryContainer.innerHTML = '';
-	for (var i = searchHistory.length - 1; i >= 0; i--) {
-		var btn = document.createElement('button');
-		btn.setAttribute('type', 'button');
-		btn.setAttribute('aria-controls', 'today forecast');
-		btn.classList.add('history-btn', 'btn-history');
-		btn.setAttribute('data-search', searchHistory[i]);
-		btn.textContent = searchHistory[i];
-		searchHistoryContainer.append(btn);
-	}
-}
-
-function updateHistory(search) {
-	if (searchHistory.indexOf(search) !== -1) {
-		return;
-	}
-	searchHistory.push(search);
-	localStorage.setItem('search-history', JSON.stringify(searchHistory));
-	createSearchHistory();
-}
-
-function getSearchHistory() {
-	var storedHistory = localStorage.getItem('search-histroy');
-	if (storedHistory) {
-		searchHistory = JSON.parse(storedHistory);
-	}
-	createSearchHistory();
 }
 
 function forecastCard(forecast, timezone) {
@@ -115,11 +115,86 @@ function forecastCard(forecast, timezone) {
 	windEl.setAttribute('class', 'card-text');
 	humidityEl.setAttribute('class', 'card-text');
     cardTitle.textContent = dayjs.unix(unixTs).tz(timezone).format('M/D/YYYY');
-	weatherIcon.setAttribute('src', icon);
-	weatherIcon.setAttribute('alt', iconDesc);
-	tempEl.textContent = `Temp: ${tempF} °F`;
-	windEl.textContent = `Wind: ${windMph} MPH`;
+	weatherImg.setAttribute('src', icon);
+	weatherImg.setAttribute('alt', iconDesc);
+	tempEl.textContent = `Temp: ${temp} °F`;
+	windEl.textContent = `Wind: ${wind} MPH`;
 	humidityEl.textContent = `Humidity: ${humidity} %`;
 	forecastCon.append(col);
 }
 
+function renderForecast(dailyForecast, timezone) {
+	var startDate = dayjs().tz(timezone).add(1, 'day').startOf('day').unix();
+	var endDate = dayjs().tz(timezone).add(6, 'day').startOf('day').unix();
+	var headingCol = document.createElement('div');
+	var heading = document.createElement('h4');
+	headingCol.setAttribute('class', 'col-12');
+	heading.textContent = '5-Day Forecast:';
+	headingCol.append(heading);
+	forecastCon.innerHTML = '';
+	forecastCon.append(headingCol);
+	for (var i = 0; i < dailyForecast.length; i++) {
+		if (dailyForecast[i].dt >= startDate && dailyForecast[i].dt < endDate) {
+			renderForecastCard(dailyForecast[i], timezone);
+		}
+	}
+}
+
+function createItems(city, data) {
+    currentWeather(city, data.current, data.timezone);
+    renderForecast(data.daily, data.timezone);
+}
+
+function fetchWeather(location) {
+    var lat = location;
+    var lon = location;
+    var city = location.name;
+    var apiUrl = `${weatherAPI}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherAPIKey}`;
+    fetch(apiUrl).then(function(res) {
+        return res.json();
+    }).then(function(data)  {
+        createItems(city, data);        
+    }).catch(function (err) {
+        console.error(err);
+    });
+}
+
+function fetchCoords(search) {
+    var apiUrl =  `${weatherAPI}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherAPIKey}`;
+    fetch(apiUrl).then(function (res) {
+        return res.json();
+    }).then(function (data) {
+        if (!data[0]) {
+            alert('Location not found');
+        } else {
+            updateHistory(search);
+            fetchWeather(data[0]);
+        }
+    })
+    .catch(function (err) {
+        console.error(err);
+    });
+}
+
+function searchFormSubmit(e) {
+	if (!searchInput.value) {
+		return;
+	}
+	e.preventDefault();
+	var search = searchInput.value.trim();
+	fetchCoords(search);
+	searchInput.value = '';
+}
+
+function searchHistoryClick(e) {
+	// Don't do search if current elements is not a search history button
+	if (!e.target.matches('.btn-history')) {
+		return;
+	}
+	var btn = e.target;
+	var search = btn.getAttribute('data-search');
+	fetchCoords(search);
+}
+getSearchHistory();
+searchForm.addEventListener('submit', searchFormSubmit);
+searchHistoryContainer.addEventListener('click', searchHistoryClick);
